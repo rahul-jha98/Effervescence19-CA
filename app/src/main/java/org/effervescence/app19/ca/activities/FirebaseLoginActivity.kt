d3f2db10db9af0d6b9eb776790c8ad604562db64
anurag <iit2018121@iiita.ac.in>
2019-09-01 17:13:02 +0530

    Add firebase

---
+++ app/src/main/java/org/effervescence/app19/ca/activities/FirebaseLoginActivity.kt
@@ -0,0 +1,13 @@
+package org.effervescence.app19.ca.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.effervescence.app19.ca.R
+
+class FirebaseLoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_firebase_login)
+    }
+}
